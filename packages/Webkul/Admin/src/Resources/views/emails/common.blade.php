24f1355634c822796a7081c7b0f0d21a0f0f0dd4
Arun Girivasan <arungirivasan@gmail.com>
2023-09-18 13:15:25 +0530

    logo updated

--- packages/Webkul/Admin/src/Resources/views/emails/common.blade.php
+++ packages/Webkul/Admin/src/Resources/views/emails/common.blade.php
@@ -2,7 +2,7 @@
     <div style="text-align: center;">
         <a href="{{ config('app.url') }}">
             <!-- <img src="{{ asset('vendor/webkul/admin/assets/images/logo.svg') }}" alt="{{ config('app.name') }}"/> -->
-            <img src="https://www.kayool.com/assets/logo/kayool.png" alt="{{ config('app.name') }}" style="height: 50px;width: unset;"/>
+            <img src="asset('vendor/webkul/admin/assets/images/kayool-logo.png')" alt="{{ config('app.name') }}" style="height: 50px;width: unset;"/>
 
         </a>
     </div>
