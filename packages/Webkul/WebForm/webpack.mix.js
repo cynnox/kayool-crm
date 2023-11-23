9d59250c6ac3eec21f5c26b9ee8714183606dbd6
Arun Girivasan <arungirivasan@gmail.com>
2023-11-23 11:07:02 +0530

    login page responsive

--- packages/Webkul/WebForm/webpack.mix.js
+++ packages/Webkul/WebForm/webpack.mix.js
@@ -1,15 +1,17 @@
 const mix = require("laravel-mix");
 
-if (mix == 'undefined') {
+if (mix == "undefined") {
     const { mix } = require("laravel-mix");
 }
 
 require("laravel-mix-merge-manifest");
 
 if (mix.inProduction()) {
-    var publicPath = 'publishable/assets';
+    var publicPath = "publishable/assets";
 } else {
     var publicPath = "../../../public/vendor/webkul/web-form/assets";
+    // auto refresh browser while save changes
+    mix.browserSync("127.0.0.1:8000");
 }
 
 mix.setPublicPath(publicPath).mergeManifest();
@@ -17,21 +19,25 @@ mix.disableNotifications();
 
 mix.js(__dirname + "/src/Resources/assets/js/web-form.js", "js/web-form.js")
     .copy(__dirname + "/src/Resources/assets/images", publicPath + "/images")
-    .sass(__dirname + "/src/Resources/assets/sass/web-form.scss", "css/web-form.css")
+    .sass(
+        __dirname + "/src/Resources/assets/sass/web-form.scss",
+        "css/web-form.css"
+    )
     .sass(__dirname + "/src/Resources/assets/sass/app.scss", "css/admin.css")
     .options({
-        processCssUrls: false
-    }).vue();
+        processCssUrls: false,
+    })
+    .vue();
 
 mix.webpackConfig({
     resolve: {
         alias: {
-            'vue$': 'vue/dist/vue.runtime.js'
-        }
-    }
+            vue$: "vue/dist/vue.runtime.js",
+        },
+    },
 });
 
-if (! mix.inProduction()) {
+if (!mix.inProduction()) {
     mix.sourceMaps();
 }
 
