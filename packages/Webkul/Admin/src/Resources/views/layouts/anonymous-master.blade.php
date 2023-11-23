9f1d233d49c87305275bc36cbfe1a39d5e4331e9
Arun Girivasan <arungirivasan@gmail.com>
2023-11-23 13:52:48 +0530

    chat bot responsive

--- packages/Webkul/Admin/src/Resources/views/layouts/anonymous-master.blade.php
+++ packages/Webkul/Admin/src/Resources/views/layouts/anonymous-master.blade.php
@@ -5,7 +5,7 @@
     <title>@yield('page_title')</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <!-- <link rel="apple-touch-icon" sizes="57x57" href="{{ asset('vendor/webkul/admin/assets/images/favicon/apple-icon-57x57.png') }}">
